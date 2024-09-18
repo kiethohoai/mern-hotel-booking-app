179ca0fc55e1bb551145cc3c95ba431f30c70ede
kiethohoai <kietho.060193@gmail.com>
2024-09-18 12:46:02 +0700

    7. User Login API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,10 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import morgan from "morgan";
+
 import userRoutes from "./routes/user";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("🚀DB connection successful!");
@@ -13,8 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(morgan("dev"));
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀Server started on port ${PORT}`);
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,71 @@
+import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be at least 6 characters").isLength({ min: 6 }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        status: false,
+        message: errors.array(),
+      });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid credentials",
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid credentials",
+        });
+      }
+
+      // Create token
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+
+      // Send token via cookie
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+      });
+
+      // Send response
+      return res.status(200).json({
+        status: true,
+        message: "User logged in successfully",
+        userId: user._id,
+      });
+    } catch (error) {
+      console.log("🚀Error at /login route", error);
+      res.status(500).json({
+        status: false,
+        message: "Something went wrong",
+      });
+    }
+  }
+);
+
+export default router;
